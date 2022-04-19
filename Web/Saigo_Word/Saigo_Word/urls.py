a625d6b49ce1e3c42e5f76bb8af4f7980902c35f
Arc Noah <Jack.gigant@gmail.com>
2022-04-19 07:06:12 +0600

    INITIAL_Template

--- Web/Saigo_Word/Saigo_Word/urls.py
+++ Web/Saigo_Word/Saigo_Word/urls.py
@@ -1,21 +1,20 @@
-"""Saigo_Word URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from Prima_Word.views import index
+
+from django.conf.urls.static import static
+from Saigo_Word import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Prima_Word/', include('Prima_Word.urls')),
+    
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
