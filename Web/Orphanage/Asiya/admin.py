759e7a5e4d12703e25cc93507bff5751e8babb38
Arc Noah <Jack.gigant@gmail.com>
2023-05-13 07:06:36 +0600

    DB Models , Node Group

--- Web/Orphanage/Asiya/admin.py
+++ Web/Orphanage/Asiya/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 
-from .models import Node_Base,Node_Link_Base
+from .models import Node_Base,Node_Link_Base, NGCL, NG_Base
 
 admin.site.register(Node_Base)
 admin.site.register(Node_Link_Base)
+admin.site.register(NGCL)
+admin.site.register(NG_Base)
--- Web/Orphanage/Asiya/models.py
+++ Web/Orphanage/Asiya/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 
 
@@ -6,7 +7,7 @@ class Node_Base(models.Model): #Nodes data and Their Links
     Node name = Node Name
     NCV = Node Charge Value
     Node_Class = Node Type (Abstract - 2 | Constant - 1 | Faint - 0)
-        Abstract - We can't associated value, it's complexity of constant nodes (Not same as Group)
+        Abstract - We can't associated value, it's complexity of constant nodes or higher units (Not same as Group)
         Constant - It's always have some associated value behind it (Digits, Letters)
         Faint - Node with low charge (It's can't create reaction with other nodes, but can be activated by other nodes)
 
@@ -61,6 +62,70 @@ class Node_Link_Base(models.Model): #Node Links Data
         verbose_name = 'Нод Связь'
         ordering = ['last_activation']
 
+# NG_Base is some kind of empty shell that makes abstraction easier to understand
+# NGCL is the main part of Node Group, it contains Nodes and Links that formed our group
+
+class NGCL(models.Model):
+    """
+    This NGCL can be reused if there is word that have same links and nodes.
+    NGCL = Node Group Construct List
+    - NG_Code (Should contain in name, nodes key, and links pairs)
+    - NCL(Node Constructs List)
+    - NLCL(Node Link Construct list) 
+    """
+
+    ng_code = models.CharField(max_length=200, verbose_name='Код Группы', primary_key=True)
+    ncl = models.ManyToManyField(Node_Base,related_name='Node_Construct_List',verbose_name="Конструкт Лист Нодов")
+    nlcl = models.ManyToManyField(Node_Link_Base,related_name='Node_Link_Construct_List',verbose_name="Конструкт Лист Нод Линков")
+
+    def __str__(self):
+        return "NG Code [%s]" % (self.ng_code)
+
+    class Meta:
+        verbose_name_plural = 'Конструкты Нод Группы'
+        verbose_name = 'Конструкт Нод Группы'
+        ordering = ['ng_code']
+
 
 class NG_Base(models.Model):
-    pass
+    """
+    GN = Group Name
+    NGCL = Node Group Construct List :
+    - NCL(Node Constructs List)
+    - NLCL(Node Link Construct list)
+    Act_Amount = Number of Activations
+    Distribution = Amount of clusters that contain this group
+    NGX = Node_Group X Cord (By default it's 0)
+    NGY = Node_Group X Cord (By default it's 0)
+    NGZ = Node_Group X Cord (By default it's 0)
+    """
+    
+    group_name = models.CharField(max_length=20,verbose_name='Название Группы',primary_key=True)
+
+    ngcl = models.ForeignKey(NGCL,related_name='Node_Group_Construct_List', on_delete=models.CASCADE, verbose_name="Конструкты Нод Группы")
+
+    class StatusChoice(models.TextChoices):
+        SOLID = 'solid', 'Цельная'
+        BROKEN = 'broken', 'Сломанная'
+
+    status = models.CharField(max_length = 6,verbose_name='статус',choices=StatusChoice.choices)
+    act_amount = models.FloatField(verbose_name='Активации',default=1)
+    distribution = models.FloatField(verbose_name='Распространенность',default=0)
+
+    ngx = models.FloatField(verbose_name='X координата',default=0.0)
+    ngy = models.FloatField(verbose_name='Y координата',default=0.0)
+    ngz = models.FloatField(verbose_name='Z координата',default=0.0)
+
+    
+
+    ascension_date = models.DateTimeField(verbose_name="Дата Формирования", auto_now_add=True, null=True)
+    last_activation = models.DateTimeField(verbose_name='Последняя Активация', auto_now_add=True,null=True)
+
+
+    def __str__(self):
+        return "NG - [%s] | Status - [%s] | NGX - [%s] | NGY - [%s] | NGZ - [%s]" % (self.group_name, self.status, self.ngx, self.ngy, self.ngz)
+
+    class Meta:
+        verbose_name_plural = 'Нод Группы'
+        verbose_name = 'Нод Группа'
+        ordering = ['last_activation']
